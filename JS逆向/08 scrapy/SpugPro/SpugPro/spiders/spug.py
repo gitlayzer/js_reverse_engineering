be8afff2339b2ca59cca7749e3042617c7408145
ziyangyang <ziyang.yang@aliyun.com>
2024-03-18 13:04:37 +0800

    重写start_request

---
+++ JS逆向/08 scrapy/SpugPro/SpugPro/spiders/spug.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.http import Request
+
+
+class SpugSpider(scrapy.Spider):
+    name = "spug"
+    allowed_domains = ["*"]
+    start_urls = ["https://devops.psi-gene.com/api/app"]
+
+    # 重写start_request 方法，发送post请求，获取cookie
+    def start_requests(self):
+        # post请求,携带请求体数据
+        login_url = 'https://devops.psi-gene.com/api/account/login/'
+        # 使用request进行请求
+        # 使用Request子类FormRequest进行请求  自动为post请求
+        yield scrapy.FormRequest(
+            url=login_url,
+            formdata={'username': 'admin', 'password': 'Admin@1024', 'type': 'default'},
+            callback=self.do_start
+        )
+
+    def do_start(self, response):
+        '''
+                登陆成功后调用parse进行处理
+                cookie中间件会帮我们自动处理携带cookie
+                '''
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response, **kwargs):
+        print(response.text)
