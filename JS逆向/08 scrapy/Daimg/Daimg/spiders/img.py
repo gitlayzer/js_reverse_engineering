945c08814dc99604ccea1eda0411eabbfd366ba7
ziyangyang <ziyang.yang@aliyun.com>
2024-03-18 16:08:53 +0800

    全站爬取

--- JS逆向/08 scrapy/Daimg/Daimg/spiders/img.py
+++ JS逆向/08 scrapy/Daimg/Daimg/spiders/img.py
@@ -12,15 +12,16 @@ class ImgSpider(CrawlSpider):
     # 匹配页码link 规则
     link = LinkExtractor(allow=r'/pic/%E6%B1%BD%E8%BD%A6-0-0-0-0-0_\d+.html', )
     # 获取每个页面重所有别的img标签信息
-    link2 = LinkExtractor(tags=['img'], attrs=['src'], deny_extensions=[''])
+    # link2 = LinkExtractor(tags=['img'], attrs=['src'], deny_extensions=[''])
+    link2 = LinkExtractor(restrict_xpaths=['/html/body/div[5]/ul/li'], deny_extensions=[''])
     rules = (
         # 配置规则,递归获取所有的页面链接,执行回调函数，获取相应,获取img标签不需要递归，设置follow = false
         Rule(link2, callback='parse_item', follow=False, ),
-        Rule(link, follow=True,),
+        Rule(link, follow=True, ),
 
     )
 
     def parse_item(self, response):
-        print(response)
+        print(response.url)
     # def parse(self, response, **kwargs):
     #     pass
