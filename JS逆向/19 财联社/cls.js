2f6b00f5dc3a53c1769bd215f4c2787124a35b2f
ziyangyang <ziyang.yang@aliyun.com>
2024-03-29 15:07:28 +0800

    财联社

---
+++ JS逆向/19 财联社/cls.js
@@ -0,0 +1,9 @@
+
+require("C:\\Users\\杨子洋\\PycharmProjects\\spiders\\JS逆向\\19 财联社\\env")
+require("C:\\Users\\杨子洋\\PycharmProjects\\spiders\\JS逆向\\19 财联社\\mod1")
+require("C:\\Users\\杨子洋\\PycharmProjects\\spiders\\JS逆向\\19 财联社\\2")
+require("C:\\Users\\杨子洋\\PycharmProjects\\spiders\\JS逆向\\19 财联社\\3")
+require("C:\\Users\\杨子洋\\PycharmProjects\\spiders\\JS逆向\\19 财联社\\loader")
+
+var params = {os: 'web', sv: '7.7.5', app: 'CailianpressWeb'}
+console.log(window.loader('W2Yj')(params))
---
+++ JS逆向/19 财联社/env.js
@@ -0,0 +1,4 @@
+window = global
+
+delete global
+delete Buffer
